0f9b124d8f7b2ea0ae425993e5a33911b3105426
Darkod188 <138383515+Darkod188@users.noreply.github.com>
2024-02-15 01:42:16 -0500

    Create OpenweatherControllerTest.java

---
+++ proyecto - copia/src/test/java/com/proyecto/proyecto/OpenweatherControllerTest.java
@@ -0,0 +1,44 @@
+package com.proyecto.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OpenweatherControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetWeather() throws Exception {
+        String cityName = "London";
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/weather")
+                .param("cityName", cityName))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8"));
+    }
+
+    @Test
+    public void testGetForecast() throws Exception {
+        String cityName = "Canada";
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/forecast")
+                .param("cityName", cityName))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8"));
+    }
+
+    @Test
+    public void testGetAirPollution() throws Exception {
+        String cityName = "Portugal";
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/airpolution")
+                .param("cityName", cityName))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8"));
+    }
+}
