440d9b2b8e56f2bcb35e08d13304ee8d58d59eda
Darkod188 <138383515+Darkod188@users.noreply.github.com>
2024-02-15 15:57:38 -0500

    Add files via upload

---
+++ proyecto - copia/src/test/java/com/proyecto/proyecto/service/OpenweatherServiceTest.java
@@ -0,0 +1,47 @@
+package com.proyecto.proyecto.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
+public class OpenweatherServiceTest {
+
+    @InjectMocks
+    private OpenweatherService openweatherService;
+
+    @Mock
+    private ConsultaService consultaService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    void testGetCurrentWeather() {
+        String cityName = "New York";
+        String respuestaWeather = openweatherService.getCurrentWeather(cityName);
+        assertNotNull(respuestaWeather);
+
+    }
+
+    @Test
+	void testGetForecast() {
+		String cityName = "Medellin";
+        String respuestaForecast = openweatherService.getForecast(cityName);
+        assertNotNull(respuestaForecast);
+	}
+
+	@Test
+	void testGetAirPollution() {
+		String cityName = "Medellin";
+        String respuestaAirPolution = openweatherService.getAirPollution(cityName);
+        assertNotNull(respuestaAirPolution);
+	}
+}
