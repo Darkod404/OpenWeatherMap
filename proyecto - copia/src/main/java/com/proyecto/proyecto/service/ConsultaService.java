ea0fa0d647ae7406951d2c8d40d053322acc9d66
Darkod188 <138383515+Darkod188@users.noreply.github.com>
2024-02-16 02:16:33 -0500

    Update ConsultaService.java

--- proyecto - copia/src/main/java/com/proyecto/proyecto/service/ConsultaService.java
+++ proyecto - copia/src/main/java/com/proyecto/proyecto/service/ConsultaService.java
@@ -4,10 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.proyecto.proyecto.entity.Consulta;
 import com.proyecto.proyecto.repository.ConsultaRepository;
+import com.proyecto.proyecto.security.entity.Usuario;
+import com.proyecto.proyecto.security.entity.UsuarioPrincipal;
+
 
 @SuppressWarnings("null")
 @Service
@@ -16,13 +20,8 @@ public class ConsultaService {
     @Autowired
     private ConsultaRepository consultaRepository;
 
-    public Consulta guardarConsulta(String cityName, String peticion, String respuesta) {
 
-        Consulta consulta = new Consulta();
-        consulta.setFechaCreacion(new Date());
-        consulta.setCiudad(cityName);
-        consulta.setPeticion(peticion);
-        consulta.setRespuesta(respuesta);
+    public Consulta guardarConsulta(Consulta consulta) {
         return consultaRepository.save(consulta);
     }
 
@@ -38,5 +37,20 @@ public class ConsultaService {
         consultaRepository.deleteById(id);
     }
 
+    public void saveConsulta(String cityName, String peticion, String respuesta) {
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        Usuario usuario = new Usuario();
+        usuario.setId(usuarioPrincipal.getId());
 
+        Consulta consulta = new Consulta();
+        consulta.setFechaCreacion(new Date());
+        consulta.setCiudad(cityName);
+        consulta.setPeticion(peticion);
+        consulta.setRespuesta(respuesta);
+        consulta.setUsuario(usuario);
+
+        guardarConsulta(consulta);
+    }
+    
 }
